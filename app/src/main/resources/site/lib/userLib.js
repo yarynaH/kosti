0079744bcad14d0b6f442df1d43ebc91a9cebb95
Maksym Yurchenko <mvy@norse.co>
2019-02-02 18:55:51 +0200

    added user register and login backend

--- app/src/main/resources/site/lib/userLib.js
+++ app/src/main/resources/site/lib/userLib.js
@@ -1,30 +1,80 @@
 var contentLib = require('/lib/xp/content');
 var portal = require('/lib/xp/portal');
-var thymeleaf = require('/lib/xp/thymeleaf');
 var norseUtils = require('norseUtils');
 var authLib = require('/lib/xp/auth');
-var nodeLib = require('/lib/xp/node');
-var libs = {
-    content: require('/lib/xp/content'),
-    context: require('/lib/xp/context')
-};
+var contextLib = require('/lib/contextLib');
 
 exports.getCurrentUser = function(){
 	var user = authLib.getUser();
 	var userObj = false;
 	if( user && user.email && user.displayName ){
-	  userObj = contentLib.query({
-	    query: "data.email = '" + user.email + "' AND displayName = '" + user.displayName + "'",
-	    contentTypes: [
-	        app.name + ":user"
-	    ]
-	  });
-	  if( userObj.hits && userObj.hits[0] ){
-	    userObj = userObj.hits[0];
-	    userObj.image = norseUtils.getImage( userObj.data.userImage, 'block(32,32)' );
-	  } else {
-	    userObj = false;
-	  }
+		contextLib.runAsAdmin(function () {
+			userObj = contentLib.query({
+				query: "data.email = '" + user.email + "' AND displayName = '" + user.displayName + "'",
+				contentTypes: [
+					app.name + ":user"
+				]
+			});
+		});
+		if( userObj.hits && userObj.hits[0] ){
+			userObj = userObj.hits[0];
+			userObj.image = norseUtils.getImage( userObj.data.userImage, 'block(32,32)' );
+		} else {
+			userObj = false;
+		}
 	}
 	return userObj;
+}
+
+exports.register = function( name, mail, pass ){
+    var site = portal.getSite();
+	var user = authLib.createUser({
+	    userStore: 'system',
+	    name: name,
+	    displayName: name,
+	    email: mail
+	});
+	authLib.changePassword({
+	    userKey: user.key,
+	    password: pass
+	});
+	var userObj = this.createUserContentType( name, mail );
+	if( userObj ){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+exports.createUserContentType = function( name, mail ){
+    var site = portal.getSiteConfig();
+    var usersLocation = contentLib.get({ key: site.userLocation });
+	var user = contentLib.create({
+		parentPath: usersLocation._path,
+		name: encodeURI(name),
+		displayName: name,
+		branch: 'draft',
+    	contentType: app.name + ':user',
+    	language: 'ru',
+    	data: {
+    		email: mail
+    	}
+	});
+	var result = contentLib.publish({
+	    keys: [user._id],
+	    sourceBranch: 'draft',
+	    targetBranch: 'master'
+	});
+	return result;
+}
+
+exports.login = function( name, pass ){
+	var loginResult = authLib.login({
+	    user: name,
+	    password: pass,
+	    userStore: 'system'
+	});
+	if( loginResult.authenticated == true ){
+		return this.getCurrentUser();
+	}
 }
--- app/src/main/resources/site/services/user/user.js
+++ app/src/main/resources/site/services/user/user.js
@@ -1,15 +1,29 @@
-var libs = {
-    norseUtils: require('norseUtils'),
-    content: require('/lib/xp/content'),
-    context: require('/lib/xp/context')
-};
+var norseUtils = require('norseUtils');
+var userLib = require('userLib');
+var portal = require('/lib/xp/portal');
+var contextLib = require('/lib/contextLib');
 var contentLib = require('/lib/xp/content');
 
 exports.get = function( req ) {
     var result = false;
-    libs.norseUtils.log('12345');
     return {
         body: result,
         contentType: 'text/html'
+    }
+};
+
+exports.post = function( req ) {
+	var result = false;
+    var params = JSON.parse(req.body);
+    if( params.action == 'register' ){
+	    contextLib.runAsAdmin(function () {
+		    result = userLib.register( params.name, params.email, params.pass );
+	    });
+    } else if( params.action == 'login' ){
+	    result = userLib.login( params.name, params.pass );
+	}
+    return {
+        body: result,
+        contentType: 'application/json'
     }
 };
