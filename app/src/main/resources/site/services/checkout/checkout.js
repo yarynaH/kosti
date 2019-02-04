1ecdd337e7288c3d67f437fd6840ef4d3bc21af4
Maksym Yurchenko <mvy@norse.co>
2019-02-04 22:42:25 +0200

    added zoomable image. pdp validation and error message

--- app/src/main/resources/site/services/checkout/checkout.js
+++ app/src/main/resources/site/services/checkout/checkout.js
@@ -5,4 +5,8 @@ var contextLib = require('/lib/contextLib');
 var contentLib = require('/lib/xp/content');
 
 exports.post = function( req ) {
+    return {
+        body: JSON.stringify(req, undefined, 4),
+        contentType: 'text/html'
+    }
 };
