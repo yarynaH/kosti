dbf685762403e62a4943bdb4ebc3929f06d003d3
Maksym Yurchenko <mvy@norse.co>
2019-02-07 21:18:47 +0200

    checkout rework

--- app/src/main/resources/site/services/checkout/checkout.js
+++ app/src/main/resources/site/services/checkout/checkout.js
@@ -8,17 +8,18 @@ var thymeleaf = require('/lib/xp/thymeleaf');
 
 exports.post = function( req ) {
 	var params = req.params;
+    var view = resolve('checkout.html');
 	if( params.step && params.step == '2' ){
-	    var view = resolve('stepTwo.html');
-	    var model = createStepTwoModel( params );
+        var stepView = thymeleaf.render( resolve('stepTwo.html'), createStepTwoModel( params ));
 	} else if( params.step && params.step == '3' ){
 	} else {
-	    var view = resolve('stepOne.html');
-	    var model = createStepOneModel( params );
+        var stepView = thymeleaf.render( resolve('stepOne.html'), createStepOneModel( params ));
 	}
+    var model = getCheckoutMainModel( params );
+    model.stepView = stepView;
     return {
-      body: thymeleaf.render(view, model),
-      contentType: 'text/html'
+        body: thymeleaf.render(view, model),
+        contentType: 'text/html'
     };
 
     function createStepOneModel( params ) {
@@ -29,7 +30,6 @@ exports.post = function( req ) {
         	productId: params.productId,
         	total: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
         	productsTotal: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
-            pageComponents: helpers.getPageComponents(req),
             quantity: params.quantity,
             product: product
         };
@@ -39,12 +39,24 @@ exports.post = function( req ) {
         var product = contentLib.get({ key: params.productId });
         product.image = norseUtils.getImage( product.data.mainImage, 'block(73, 73)' );
         return {
-            pageComponents: helpers.getPageComponents(req),
             cartProductsTotal: params.quantity,
             product: product,
             total: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
             productsTotal: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
             productId: params.productId
+        };
+    }
+
+    function getCheckoutMainModel( params ){
+        var product = contentLib.get({ key: params.productId });
+        product.image = norseUtils.getImage( product.data.mainImage, 'block(73, 73)' );
+        return {
+            cartProductsTotal: params.quantity,
+            product: product,
+            total: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
+            productsTotal: (parseInt(params.quantity) * parseInt(product.data.price)).toFixed(),
+            productId: params.productId,
+            pageComponents: helpers.getPageComponents(req)
         };
     }
 };
