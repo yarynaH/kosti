c5a81322dec9375fefb493a2df7c580841f32246
Maksym Yurchenko <mvy@norse.co>
2019-04-28 00:01:10 +0300

    conflict

--- app/src/main/resources/site/services/user/user.js
+++ app/src/main/resources/site/services/user/user.js
@@ -4,6 +4,15 @@ var mailsLib = require('mailsLib');
 var portal = require('/lib/xp/portal');
 var contextLib = require('/lib/contextLib');
 var contentLib = require('/lib/xp/content');
+var helpers = require('helpers');
+var thymeleaf = require('/lib/xp/thymeleaf');
+
+var templates = {
+    forgotPassForm: "forgotPassForm.html",
+    resetFailed: "resetFailed.html",
+    unsubscribe: "unsubscribe.html",
+    userActivation: "userActivation.html"
+}
 
 exports.get = function( req ) {
     var params = req.params;
@@ -16,6 +25,7 @@ exports.get = function( req ) {
             break;
         case "newsletterUnsubscribe":
             mailsLib.unsubscribe( params.hash );
+            view = resolve(templates.forgotPassForm);
             break;
         default:
             break;
