e270527c4119ddb2cb95bbb719ff303126fe1ac4
Maksym Yurchenko <mvy@norse.co>
2019-05-04 18:20:12 +0300

    form

--- app/src/main/resources/site/lib/formLib.js
+++ app/src/main/resources/site/lib/formLib.js
@@ -16,23 +16,28 @@ function submitForm( params ){
 }
 
 function checkSpace( params ){
-  var formNode = connectFormRepo();
-  var result = formNode.query({
-    query: params.game + " = '" + params.name + "'"
+  var result;
+  contextLib.runAsAdmin(function () {
+    var formNode = connectFormRepo();
+    result = formNode.query({
+      query: params.game + " = '" + params.name + "'"
+    });
   });
   return result.total;
 }
 
 function getForms( formType ){
-  var formNode = connectFormRepo();
   var result = [];
-  var hits = formNode.query({
-    query: "formType = '" + formType + "'",
-    count: 99999999
-  }).hits;
-  for( var i = 0; i < hits.length; i++ ){
-    result.push(formNode.get(hits[i].id));
-  }
+  contextLib.runAsAdmin(function () {
+    var formNode = connectFormRepo();
+    var hits = formNode.query({
+      query: "formType = '" + formType + "'",
+      count: 99999999
+    }).hits;
+    for( var i = 0; i < hits.length; i++ ){
+      result.push(formNode.get(hits[i].id));
+    }
+  });
   return result;
 
 }
