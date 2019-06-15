f2e4661107ef9b2d16c2b06df125ec257b062e82
Maksym Yurchenko <mvy@norse.co>
2019-06-15 21:29:50 +0300

    fix to shipping adr

---
+++ app/src/main/resources/site/lib/notificationLib.js
@@ -0,0 +1,13 @@
+var norseUtils = require('norseUtils');
+var contentLib = require('/lib/xp/content');
+var portalLib = require('/lib/xp/portal');
+var nodeLib = require('/lib/xp/node');
+var contextLib = require('contextLib');
+var userLib = require('userLib');
+var kostiUtils = require('kostiUtils');
+var sharedLib = require('sharedLib');
+
+//types: bookmark, like, comment
+//content id: article, comment
+function addNotification( content, type ){
+}
