a27bd65c59cada23ab8bf9fb92e59c7046f797a0
Maksym Yurchenko <mvy@norse.co>
2019-11-23 19:31:23 +0200

    monster creation

--- app/src/main/resources/services/newMonster/newMonster.js
+++ app/src/main/resources/services/newMonster/newMonster.js
@@ -1,5 +1,4 @@
 var thymeleaf = require("/lib/thymeleaf");
-var authLib = require("/lib/xp/auth");
 var portal = require("/lib/xp/portal");
 var contentLib = require("/lib/xp/content");
 var i18nLib = require("/lib/xp/i18n");
@@ -7,53 +6,48 @@ var i18nLib = require("/lib/xp/i18n");
 var libLocation = "../../site/lib/";
 var norseUtils = require(libLocation + "norseUtils");
 var helpers = require(libLocation + "helpers");
-var userLib = require(libLocation + "userLib");
-var spellLib = require(libLocation + "spellsLib");
-var articlesLib = require(libLocation + "articlesLib");
-var blogLib = require(libLocation + "blogLib");
 
 exports.get = handleGet;
+exports.put = handlePut;
+
+function handlePut(req) {
+  createArticle(req.params);
+  return renderView(req);
+
+  function createArticle(data) {
+    var site = portal.getSiteConfig();
+    var monstersLocation = contentLib.get({ key: site.monstersLocation });
+    var displayName = data.name;
+    var name = common.sanitize(data.name);
+    delete data.name;
+    return contextLib.runInDraftAsAdmin(function() {
+      var result = contentLib.create({
+        name: name,
+        parentPath: monstersLocation._path,
+        displayName: displayName,
+        contentType: app.name + ":monster",
+        data: data
+      });
+      return result;
+    });
+  }
+}
 
 function handleGet(req) {
-  var me = this;
+  return renderView(req);
+}
 
-  function renderView() {
-    var view = resolve("newMonster.html");
-    var user = userLib.getCurrentUser();
-    if (!user) {
-      return false;
-    }
-    var model = createModel();
-    var body = thymeleaf.render(view, model);
-    return {
-      body: body,
-      contentType: "text/html"
-    };
-  }
-
-  function createModel() {
-    var up = req.params;
-    if (!up) {
-      up = {};
-    }
-    var content = portal.getContent();
-    var response = [];
-    var site = portal.getSiteConfig();
-
-    var model = {
-      content: content,
-      app: app,
-      site: site,
-      agreementPage: portal.pageUrl({
-        id: portal.getSiteConfig().agreementPage
-      }),
-      data: up,
-      social: site.social,
-      pageComponents: helpers.getPageComponents(req, null, null, "Новая статья")
-    };
-
-    return model;
-  }
-
-  return renderView();
+function renderView(req) {
+  var view = resolve("newMonster.html");
+  var site = portal.getSiteConfig();
+  var body = thymeleaf.render(view, {
+    app: app,
+    site: site,
+    social: site.social,
+    pageComponents: helpers.getPageComponents(req, null, null, "Новая статья")
+  });
+  return {
+    body: body,
+    contentType: "text/html"
+  };
 }
