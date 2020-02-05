cf54e30e616c4a46da031086cdbf561789492578
Maksym Yurchenko <mvy@norse.co>
2020-02-05 17:03:03 +0200

    minor fixes for podcast

--- app/src/main/resources/services/podcast/podcast.js
+++ app/src/main/resources/services/podcast/podcast.js
@@ -10,6 +10,7 @@ var libLocation = "../../site/lib/";
 var norseUtils = require(libLocation + "norseUtils");
 var contextLib = require(libLocation + "contextLib");
 var helpers = require(libLocation + "helpers");
+var moment = require(libLocation + "moment");
 
 exports.get = function(req) {
   var podcasts = contentLib.query({
@@ -33,8 +34,7 @@ exports.get = function(req) {
   function beautifyEpisode(episode) {
     let type = episode.attachments[episode.data.audioFile].mimeType;
     let size = episode.attachments[episode.data.audioFile].size;
-    let date = new Date(episode.publish.from);
-    date = date.toUTCString();
+    let date = moment(episode.publish.from).toString();
     let fileUrl = portal.attachmentUrl({
       id: episode._id,
       type: "absolute",
@@ -46,30 +46,66 @@ exports.get = function(req) {
       guid: episode._id,
       link: portal.pageUrl({ id: episode._id, type: "absolute" }),
       pubDate: date,
-      "itunes:explicit": episode.data.explicit,
+      explicit: episode.data.explicit,
       enclosure: { url: fileUrl, length: size, type: type },
       "itunes:episode": episode.data.episode,
-      "itunes:season": episode.data.season
+      "itunes:season": episode.data.season,
+      "itunes:explicit": episode.data.explicit,
+      episode: episode.data.episode,
+      season: episode.data.season,
+      image: {
+        link: portal.pageUrl({ id: episode._id, type: "absolute" }),
+        title: episode.displayName,
+        url: norseUtils.getImage(
+          episode.data.image,
+          "block(1400,1400)",
+          null,
+          "absolute"
+        ).url
+      },
+      "itunes:image": ""
     };
   }
 
   function createPodcast(episodes) {
     let year = new Date().getFullYear();
+    let imageUrl = portal.assetUrl({
+      path: "images/extended-logo-min.png",
+      type: "absolute"
+    });
     return {
       title: "ЭНОА",
       link: "https://www.kostirpg.com",
       language: "ru-ru",
       copyright: "&#169; " + year + " Вечерние Кости",
-      "itunes:author": "Вечерние Кости",
+      author: "Вечерние Кости",
+      "googleplay:author": "Вечерние Кости",
       description: "Новая кампания по ЭНОА от Вечерние Кости.",
+      "itunes:author": "Вечерние Кости",
       "itunes:type": "serial",
       "itunes:owner": {
         "itunes:name": "Вечерние Кости",
         "itunes:email": "info@kostirpg.com"
       },
-      "itunes:image": "",
+      owner: {
+        name: "Вечерние Кости",
+        email: "info@kostirpg.com"
+      },
+      image: {
+        link: "https://www.kostirpg.com",
+        title: "ЭНОА",
+        url: imageUrl
+      },
+      "itunes:image": imageUrl,
       "itunes:explicit": "true",
-      "itunes:category": "hobbies",
+      "googleplay:explicit": "true",
+      "itunes:category": {
+        text: "Leisure",
+        "itunes:category": { text: "Hobbies" }
+      },
+      "googleplay:category": {
+        text: "Games &amp; Hobbies"
+      },
       item: episodes
     };
   }
@@ -77,6 +113,13 @@ exports.get = function(req) {
   function OBJtoXML(obj) {
     var xml = "";
     for (var prop in obj) {
+      if (prop === "itunes:category") {
+        xml += getCategoryText(obj[prop]);
+        continue;
+      } else if (prop === "itunes:image") {
+        xml += getImageTag(obj[prop]);
+        continue;
+      }
       xml += obj[prop] instanceof Array ? "" : "<" + prop + ">";
       if (obj[prop] instanceof Array) {
         for (var array in obj[prop]) {
@@ -93,5 +136,18 @@ exports.get = function(req) {
     }
     var xml = xml.replace(/<\/?[0-9]{1,}>/g, "");
     return xml;
+  }
+
+  function getCategoryText(category) {
+    let result = '<itunes:category text="' + category.text + '">';
+    if (category["itunes:category"]) {
+      result += getCategoryText(category["itunes:category"]);
+    }
+    result += "</itunes:category>";
+    return result;
+  }
+
+  function getImageTag(image) {
+    return '<itunes:image href="' + image + '"></itunes:image>';
   }
 };
