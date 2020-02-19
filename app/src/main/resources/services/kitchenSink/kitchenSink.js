6e1b605975be6540b8eab52c3348d12525bc24ed
Maksym Yurchenko <mvy@norse.co>
2020-02-19 20:49:48 +0200

    old pdf

--- app/src/main/resources/services/kitchenSink/kitchenSink.js
+++ app/src/main/resources/services/kitchenSink/kitchenSink.js
@@ -16,9 +16,16 @@ exports.get = function(req) {
   var qr = qrLib(4, "L");
   qr.addData("KOSTICON2020");
   qr.make();
-  var html = thymeleaf.render(resolve("kitchenSink.html"), {
-    qrcode: qr.createTableTag(15, 0)
-  });
+  var html = thymeleaf.render(
+    /*resolve("kitchenSink.html"),
+    {
+      qrcode: qr.createTableTag(15, 0)
+    }*/
+    resolve("../../site/pages/pdfs/regularTicket.html"),
+    {
+      qrcode: qr.createTableTag(7, 0)
+    }
+  );
   if (req.params.pdf) {
     var bean = __.newBean("com.myurchenko.lib.pdf.PdfHandler");
     return {
