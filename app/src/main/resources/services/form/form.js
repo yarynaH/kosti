0da4a0c846d61c4b61a7fe1fecfc6138b2d85016
Maksym Yurchenko <mvy@norse.co>
2020-03-23 17:58:38 +0200

    gm backend

--- app/src/main/resources/services/form/form.js
+++ app/src/main/resources/services/form/form.js
@@ -1,5 +1,4 @@
 var portal = require("/lib/xp/portal");
-var contextLib = require("/lib/contextLib");
 var contentLib = require("/lib/xp/content");
 var thymeleaf = require("/lib/thymeleaf");
 
@@ -7,21 +6,64 @@ var libLocation = "../../site/lib/";
 var norseUtils = require(libLocation + "norseUtils");
 var formLib = require(libLocation + "formLib");
 
-exports.post = function(req) {
-  var action = req.params.action;
-  delete req.params.action;
+exports.get = function(req) {
   var result = {};
-  switch (action) {
+  switch (req.params.action) {
     case "addGame":
+      var data = {
+        displayName: "new game",
+        blockId: "b0b239d3-f7e9-4abb-a286-ca0c151479fc",
+        description: "new game is super cool",
+        maxPlayers: 4,
+        location: "13f3a27a-e726-4cdd-a96b-c8e30c92c415",
+        kidsGame: true,
+        explicit: false,
+        gameSystem: { select: { system: "dnd" }, _selected: "select" },
+        masterName: "max"
+      };
+      formLib.addGame(data);
       /*
-        title: displayName and _name
+        displayName: displayName and _name
         blockId: parent, where data is created
         description
         maxPlayers
+        locationId
         kidsGame: true || false
+        explicit: true || false
         system: game system(dnd, vtm, etc.)
         masterName
         masterNickname
+      */
+      break;
+    case "editGame":
+      var data = {
+        _id: "82f0b63d-27b8-4319-bfd5-cba2b99778cc",
+        displayName: "new game",
+        description: "new game is super cool",
+        maxPlayers: 4,
+        kidsGame: true,
+        explicit: false,
+        gameSystem: { select: { system: "dnd" }, _selected: "select" },
+        masterName: "ivan"
+      };
+      formLib.modifyGame(data);
+      /*
+        _id
+        displayName: displayName and _name
+        description
+        maxPlayers
+        kidsGame: true || false
+        explicit: true || false
+        system: game system(dnd, vtm, etc.)
+        masterName
+        masterNickname
+      */
+      break;
+    case "deleteGame":
+      var id = "ffebc45a-3b73-4cd1-816f-8b62883509e1";
+      formLib.deleteGame(id);
+      /*
+        id
       */
       break;
     default:
