0c0cde70d4991ba994f46a8f8ed77ec6dd347159
Maksym Yurchenko <mvy@norse.co>
2020-08-21 14:06:06 +0300

    fixed es15 issue

--- frontend/app/js/sharedLib.js
+++ frontend/app/js/sharedLib.js
@@ -36,8 +36,8 @@ function getCookieValueOld(a) {
 }
 
 function getCookieValue(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
+  var value = `; ${document.cookie}`;
+  var parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
