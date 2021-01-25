177d46434d1cc25d79c0f26e0ea7746de88f5f5c
Maksym Yurchenko <mvy@norse.co>
2021-01-25 20:44:58 +0200

    added generation of filters

--- app/src/main/resources/site/pages/games/games.js
+++ app/src/main/resources/site/pages/games/games.js
@@ -35,10 +35,31 @@ function handleReq(req) {
       user: user,
       days: days,
       festival: festival,
+      filters: getFilters(),
       pageComponents: helpers.getPageComponents(req)
     };
 
     return model;
+  }
+
+  function getFilters() {
+    let filters = { themes: [], system: [] };
+    let festival = formSharedLib.getActiveFestival();
+    let games = formSharedLib.getItemsList({
+      parentId: festival._id,
+      type: "game",
+      parentPathLike: true
+    });
+    games.forEach((game) => {
+      if (game.data.theme && filters.themes.indexOf(game.data.theme) === -1)
+        filters.themes.push(game.data.theme);
+      if (
+        game.data.gameSystem.select.system &&
+        filters.system.indexOf(game.data.gameSystem.select.system) === -1
+      )
+        filters.system.push(game.data.gameSystem.select.system);
+    });
+    return filters;
   }
 
   return renderView();
