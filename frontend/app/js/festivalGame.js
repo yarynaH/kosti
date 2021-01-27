39731e998dc2c3edda4c2e40aa9e9e319f586cee
Maksym Yurchenko <mvy@norse.co>
2021-01-27 14:14:01 +0200

    adde missing games

---
+++ frontend/app/js/festivalGame.js
@@ -0,0 +1,10 @@
+function initKosticonnetcScripts() {
+  $(".js_sign-up-for-game").on("click", function (e) {
+    e.preventDefault();
+    console.log("test");
+  });
+}
+
+$(document).ready(function () {
+  initKosticonnetcScripts();
+});
