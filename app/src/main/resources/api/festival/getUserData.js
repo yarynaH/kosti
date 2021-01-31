15b2c2c55fa63db01564473d2c9af58eb82dae09
Maksym Yurchenko <mvy@norse.co>
2021-01-31 15:59:40 +0200

    game register

---
+++ app/src/main/resources/api/festival/getUserData.js
@@ -0,0 +1,26 @@
+var portal = require("/lib/xp/portal");
+var contentLib = require("/lib/xp/content");
+var thymeleaf = require("/lib/thymeleaf");
+var httpClientLib = require("/lib/http-client");
+
+var libLocation = "../../site/lib/";
+var norseUtils = require(libLocation + "norseUtils");
+var userLib = require(libLocation + "userLib");
+var helpers = require(libLocation + "helpers");
+var formPlayerLib = require(libLocation + "games/formPlayerLib");
+
+exports.post = function (req) {
+  return {
+    /*body: formPlayerLib.checkTicket({
+      ticket: req.params.ticket,
+      cartId: req.cookies.cartId
+    }),*/
+    body: formPlayerLib.bookSpace({
+      cartId: req.cookies.cartId,
+      ticket: req.params.ticket,
+      firstName: req.params.firstName,
+      gameId: req.params.gameId
+    }),
+    contentType: "application/json"
+  };
+};
--- app/src/main/resources/site/lib/games/formPlayerLib.js
+++ app/src/main/resources/site/lib/games/formPlayerLib.js
@@ -5,9 +5,15 @@ const userLib = require("../userLib");
 const formSharedLib = require("formSharedLib");
 const i18nLib = require("/lib/xp/i18n");
 const util = require("/lib/util");
+const contextLib = require("../contextLib");
+const cartLib = require("../cartLib");
 
 exports.getDays = getDays;
 exports.beautifyGame = beautifyGame;
+exports.gameSpaceAvailable = gameSpaceAvailable;
+exports.bookSpace = bookSpace;
+exports.checkTicket = checkTicket;
+exports.changeCartIdToPlayerId = changeCartIdToPlayerId;
 
 function getDays(params) {
   let days = [];
@@ -106,4 +112,164 @@ function beautifyGame(game) {
   game.master = contentLib.get({ key: game.data.master });
   if (game.data.image) game.image = norseUtils.getImage(game.data.image);
   return game;
+}
+
+function gameSpaceAvailable(id) {
+  let game = contentLib.get({ key: id });
+  let players = norseUtils.forceArray(game.data.players);
+  if (parseInt(game.data.maxPlayers) > players.length) {
+    return true;
+  }
+  return false;
+}
+
+function checkTicket(params) {
+  let cart = cartLib.getCartByQr(params.ticket);
+  if (!cart) return false;
+  let user = userLib.getCurrentUser();
+  let currCart = null;
+  let userTicket =
+    user &&
+    user.data &&
+    user.data.kosticonnect2021 &&
+    user.data.kosticonnect2021
+      ? user.data.kosticonnect2021
+      : null;
+  if (userTicket && user.data.kosticonnect2021 === params.ticket) {
+    return true;
+  } else if (cart.currentFriendlyId && !cart.currentQrStatus === false) {
+    return true;
+  }
+  return false;
+}
+
+function bookSpace(params) {
+  if (!gameSpaceAvailable(params.gameId)) {
+    return false;
+  }
+  let game = contentLib.get({ key: params.gameId });
+  let players = game.data.players;
+  if (!players) {
+    players = [];
+  }
+  players = norseUtils.forceArray(players);
+  let user = userLib.getCurrentUser();
+  if (
+    user &&
+    user.data &&
+    user.data.firstName &&
+    user.data.discord &&
+    user.data.kosticonnect2021
+  ) {
+    return signForGame({ gameId: params.gameId });
+  } else if ((params.firstName || params.kosticonnect2021) && user) {
+    user = updateUser(params);
+  } else if (params.firstName && params.kosticonnect2021) {
+    if (players.indexOf(params.cartId) === -1) {
+      players.push(params.cartId);
+      game.data.players = players;
+      game = updateEntity(game);
+    }
+    let cart = cartLib.getCartById(params.cartId);
+    let cartRepo = connectCartRepo();
+    let result = cartRepo.modify({
+      key: params.cartId,
+      editor: editor
+    });
+    function editor(node) {
+      node.firstName = params.firstName;
+      node.kosticonnect2021 = params.kosticonnect2021;
+      return node;
+    }
+    return game;
+  }
+}
+
+function updateUser(params) {
+  if (!params) return false;
+  let user = userLib.getCurrentUser();
+  if (!(user && user.data)) return false;
+
+  let updateUser = false;
+  if (params.ticket && !user.data.kosticonnect2021) {
+    if (checkTicket({ ticket: params.ticket })) {
+      user.data.ticket = params.ticket;
+      updateUser = true;
+      //cartLib.markTicketUsed(params.ticket);
+    } else {
+      return false;
+    }
+  }
+  if (
+    (params.firstName && !user.data.firstName) ||
+    (params.firstName && params.firstName !== user.data.firstName)
+  ) {
+    user.data.firstName = params.firstName;
+    updateUser = true;
+  }
+  if (updateUser) user = updateEntity(user);
+  return user;
+}
+
+function signForGame(params) {
+  if (!gameSpaceAvailable(params.gameId)) {
+    return false;
+  }
+  if (!params) params = {};
+  let user = userLib.getCurrentUser();
+  let game = contentLib.get({ key: params.gameId });
+  let players = game.data.players;
+  if (!players) {
+    players = [];
+  }
+  players = norseUtils.forceArray(players);
+  if (players.indexOf(user._id) === -1) {
+    players.push(user._id);
+    game.data.players = players;
+    return updateEntity(game);
+  }
+  return game;
+}
+
+function changeCartIdToPlayerId(params) {
+  if (!params) return false;
+  let user = userLib.getCurrentUser();
+  if (!user) return false;
+  let game = contentLib.query({
+    query: "data.players = '" + params.cartId + "'"
+  });
+  if (game.total > 0) game = game.hits[0];
+  game.data.players = norseUtils.forceArray(game.data.players);
+  if (game.data.players.indexOf(params.cart) !== -1) {
+    let cart = cartLib.getCartById(params.cartId);
+    user.data.firstName = cart.firstName;
+    user.data.kosticonnect2021 = cart.kosticonnect2021;
+    updateEntity(user);
+    for (let i = 0; i < game.data.players.length; i++) {
+      if (game.data.players[i] === params.cartId)
+        game.data.players[i] = user._id;
+      break;
+    }
+    updateEntity(game);
+  }
+  return true;
+}
+
+function updateEntity(entity) {
+  return contextLib.runAsAdminAsUser(userLib.getCurrentUser(), function () {
+    entity = contentLib.modify({
+      key: entity._id,
+      editor: editor
+    });
+    function editor(c) {
+      c.data = entity.data;
+      return c;
+    }
+    contentLib.publish({
+      keys: [entity._id],
+      sourceBranch: "master",
+      targetBranch: "draft"
+    });
+    return entity;
+  });
 }
