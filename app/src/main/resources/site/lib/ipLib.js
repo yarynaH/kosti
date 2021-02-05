af8656a6496648555d5a622eaaa5ea191f7a8ea3
Maksym Yurchenko <mvy@norse.co>
2021-02-05 15:39:44 +0200

    fixed timeout on ip

--- app/src/main/resources/site/lib/ipLib.js
+++ app/src/main/resources/site/lib/ipLib.js
@@ -5,16 +5,20 @@ exports.getIpInfo = getIpInfo;
 
 function getIpInfo(ip) {
   if (!ip) return null;
-  let response = httpClientLib.request({
-    url: "http://ip-api.com/json/" + ip + "?fields=countryCode,currency",
-    method: "GET",
-    contentType: "application/json"
-  });
-  if (response.status === 200) {
-    let body = JSON.parse(response.body);
-    if (body && body.countryCode) {
-      return body;
+  try {
+    let response = httpClientLib.request({
+      url: "http://ip-api.com/json/" + ip + "?fields=countryCode,currency",
+      method: "GET",
+      contentType: "application/json"
+    });
+    if (response.status === 200) {
+      let body = JSON.parse(response.body);
+      if (body && body.countryCode) {
+        return body;
+      }
     }
+  } catch (e) {
+    return null;
   }
   return null;
 }
