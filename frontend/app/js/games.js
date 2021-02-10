f8213e8ea5911186737a8fb9e2167bf39f5e2084
yaryna <yarynaholod@gmail.com>
2021-02-10 11:41:49 +0200

    Updated filters js

--- frontend/app/js/games.js
+++ frontend/app/js/games.js
@@ -6,7 +6,7 @@ function initGamesScripts() {
     });
   }
 
-  $(".js-custom_select-button").on("click", function() {
+  $(".js-custom_select").on("click",".js-custom_select-button", function(e) {
     $(this)
     .parent()
     .toggleClass("show");
@@ -14,34 +14,43 @@ function initGamesScripts() {
 
   var selected_arr=[];
   var games_i = 0;
-  $(".js-custom_select-option").on("click", function() {
+
+  $(".js-custom_select").on("click",".js-custom_select-option", function() {
     var val =$(this).data("value");
     var text =$(this).text();
-      if (selected_arr.indexOf(val) === -1) {
-        selected_arr[games_i]=val;
-        addSelectedOption(val,text);
-        games_i++;
-      }
-
-    if($(".js-custom_select").find(".js-custom_select-selected").length !== 0) {
-      $(".js-custom_select-placeholder").hide();
-    } else {
-      $(".js-custom_select-placeholder").show();
+  
+    if (selected_arr.indexOf(val) === -1) {
+      selected_arr[games_i]=val;
+      addSelectedOption(val,text);
+      games_i++;
     }
-    console.log(selected_arr);
+
+    $(".js-custom_select-placeholder").hide();
   });
 
-  $(".js-custom_select-selected").on("click", function() {
-    console.log("ccc");
+  $(".js-custom_select").on("click",".js-custom_select-selected", function(e) {
+    e.stopPropagation();
     var val =$(this).data("value");
     var index =selected_arr.indexOf(val);
+
     if (index > -1) {
       selected_arr.splice(index, 1);
     }
-    $(".js-custom_select-selected").data("value=" + val).remove();
-    console.log("value=" + val);
-    console.log(selected_arr);
+
+    $(".js-custom_select-selected[data-value="+ val + "]").remove();
+    games_i--;
+
+    if(selected_arr.length === 0) {
+      $(".js-custom_select-placeholder").show();
+    }
   })
+
+  $(document).on('click',function (e) {
+    var el = $('.js-custom_select');
+    if(!el.is(e.target) && el.has(e.target).length === 0){
+      el.removeClass("show");
+    }
+  });
 }
 
 function addSelectedOption(val,text) {
